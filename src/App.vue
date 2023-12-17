78abd4a983283ee8dc7975cf7805e7a75a5df7f0
Julian Pollinger <julian@pollinger.dev>
2023-12-18 00:34:26 +0100

    feat: change default url of ws
    BREAKING CHANGE: New default websocket url

--- src/App.vue
+++ src/App.vue
@@ -81,7 +81,7 @@ export default {
     metaThemeColor.setAttribute("content", getComputedStyle(document.body).getPropertyValue('--primary'));
 
     const loc = window.location;
-    const connectTo = (websocketURL || loc.protocol + "//" + loc.hostname + ":3420");
+    const connectTo = (websocketURL || loc.protocol + "//" + loc.hostname + "/ws");
     this.$store.commit('setSocket', io(connectTo));
     this.socket.on('info', data => {
       this.$store.commit('setServerInfo', data)
